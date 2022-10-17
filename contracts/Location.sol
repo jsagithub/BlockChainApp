1a2ac0006b287a37e57707b3cc26585312eccc47
jsa <jsa@infraspeak.com>
2022-10-17 07:14:29 +0100

    Buy Products

--- contracts/Location.sol
+++ contracts/Location.sol
@@ -3,7 +3,66 @@ pragma solidity ^0.8.16;
 contract Location{
     string public name;
 
-    constructor() public {
+    uint public productCount = 0;
+    mapping(uint => Product) public products;
+
+    struct Product {
+        uint id;
+        string name;
+        uint price;
+        address payable owner;
+        bool purchased;
+    }
+
+    event ProductCreated(
+        uint id,
+        string name,
+        uint price,
+        address payable owner,
+        bool purchased
+    );
+
+    event ProductPurchased(
+        uint id,
+        string name,
+        uint price,
+        address payable owner,
+        bool purchased
+    );
+
+    constructor() {
         name = "BlockChainApp Location";
+    }
+
+    function createProduct(string memory _name, uint _price) public {
+        require(bytes(_name).length > 0);
+        require(_price > 0);
+        productCount ++;
+        products[productCount] = Product(productCount, _name, _price, payable(msg.sender), false);
+        emit ProductCreated(productCount, _name, _price, payable(msg.sender), false);
+    }
+
+    function purchaseProduct(uint _id) public payable {
+        Product memory _product = products[_id];
+
+        address payable _seller = _product.owner;
+
+        require(_product.id > 0 && _product.id <= productCount);
+
+        require(msg.value >= _product.price);
+
+        require(!_product.purchased);
+
+        require(_seller != msg.sender);
+
+        _product.owner = payable(msg.sender);
+
+        _product.purchased = true;
+
+        products[_id] = _product;
+
+        payable(address(_seller)).transfer(msg.value);
+
+        emit ProductPurchased(productCount, _product.name, _product.price, payable(msg.sender), true);
     }
 }
--- test/Location.test.js
+++ test/Location.test.js
@@ -1,6 +1,10 @@
 const Location = artifacts.require('./contracts/Location.sol');
 
-contract('Location',(accounts) =>{
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
+
+contract('Location',([deployer, seller, buyer]) =>{
     let location
 
     before(async ()=> {
@@ -19,6 +23,65 @@ contract('Location',(accounts) =>{
         it('has a name',  async ()=> {
             const name = await location.name()
             assert.equal(name, 'BlockChainApp Location')
+        })
+    })
+
+    describe('products', async () => {
+        let result, productCount
+
+        before(async () => {
+            result = await location.createProduct('Tesla', web3.utils.toWei('1', 'Ether'), { from: seller })
+            productCount = await location.productCount()
+        })
+
+        it('creates products', async () => {
+            assert.equal(productCount, 1)
+            const event = result.logs[0].args
+            assert.equal(event.id.toNumber(), productCount.toNumber(), 'correct id type')
+            assert.equal(event.name, 'Tesla', 'correct name type')
+            assert.equal(event.price, '1000000000000000000', 'correct price type')
+            assert.equal(event.owner, seller, 'owner is correct')
+            assert.equal(event.purchased, false, 'correct purchase type')
+
+            await await location.createProduct('', web3.utils.toWei('1', 'Ether'),{ from: seller }).should.be.rejected;
+            await await location.createProduct('Tesla', 0,{ from: seller }).should.be.rejected;
+        })
+
+        it('sells products', async () => {
+            let oldSellerBalance
+            oldSellerBalance = await web3.eth.getBalance(seller)
+            oldSellerBalance = new web3.utils.BN(oldSellerBalance)
+
+
+            result = await location.purchaseProduct(productCount, { from: buyer, value: web3.utils.toWei('1', 'Ether')})
+
+            const event = result.logs[0].args
+            assert.equal(event.id.toNumber(), productCount.toNumber(), 'correct id type')
+            assert.equal(event.name, 'Tesla', 'correct name type')
+            assert.equal(event.price, '1000000000000000000', 'correct price type')
+            assert.equal(event.owner, buyer, 'owner is correct')
+            assert.equal(event.purchased, true, 'correct purchase type')
+
+            let newSellerBalance
+            newSellerBalance = await web3.eth.getBalance(seller)
+            newSellerBalance = new web3.utils.BN(newSellerBalance)
+
+            let price
+            price = web3.utils.toWei('1', 'Ether')
+            price = new web3.utils.BN(price)
+
+            const exepectedBalance = oldSellerBalance.add(price)
+
+            assert.equal(newSellerBalance.toString(), exepectedBalance.toString())
+
+
+            await location.purchaseProduct(99, { from: buyer, value: web3.utils.toWei('1', 'Ether')}).should.be.rejected;
+
+            await location.purchaseProduct(productCount, { from: buyer, value: web3.utils.toWei('0.5', 'Ether') }).should.be.rejected;
+
+            await location.purchaseProduct(productCount, { from: deployer, value: web3.utils.toWei('1', 'Ether') }).should.be.rejected;
+
+            await location.purchaseProduct(productCount, { from: buyer, value: web3.utils.toWei('1', 'Ether') }).should.be.rejected;
         })
     })
 })
